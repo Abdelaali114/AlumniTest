b334a45f6168ec8f8704336da9bbd6cad5ddab96
Abdelaali114 <abdellaali2550@gmail.com>
2024-08-26 00:06:00 +0200

    aws 3

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 import { app, server } from './socket/index.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
