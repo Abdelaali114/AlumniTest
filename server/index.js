7ebf21732363ac8d7231cfac8aaf47bc439b1553
Abdelaali114 <150744591+Abdelaali114@users.noreply.github.com>
2024-12-07 15:15:02 +0100

    index.js

--- server/index.js
+++ server/index.js
@@ -162,7 +162,7 @@ const connectDB = async () => {
 };
 
 // Listening to the requests
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
   connectDB();
   console.log("Server is running on port " + PORT);
 
